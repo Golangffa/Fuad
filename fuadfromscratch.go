0295247f47d278e96bf58a04435bc7d6d4e881d7
Dumblelord <Fuadmasimov2018@gmail.com>
2018-07-31 11:31:20 +0100

    inprocess

---
+++ fuadfromscratch.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Welcome to my website i started from stratch!")
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":80", nil)
+}
